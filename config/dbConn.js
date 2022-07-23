323f622d65a5aac97136d9f1e8ce8ee36dac3a63
unknown <oyewaleadedunmola@gmail.com>
2022-07-23 08:30:39 +0100

    add production key to config.json,  change the database url and add build key to package.json

--- config/dbConn.js
+++ config/dbConn.js
@@ -1,10 +1,4 @@
 const Sequelize = require('sequelize')
 
 
-module.exports = new Sequelize(process.env.DATABASE_NAME, 
-    process.env.DATABASE_USER, 
-    process.env.DATABASE_PASSWORD, 
-    {
-        dialect: 'postgres',
-        logging: false
-})
+module.exports = new Sequelize(process.env.DATABASE_URL)
